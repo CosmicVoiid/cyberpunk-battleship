e8fee919c6098b12f0eadf793c5ebbce270438bf
Saurav Sah <sah_64@yahoo.com>
2022-04-16 15:47:57 -0400

    fixed ship placement function

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -6,12 +6,14 @@ type tileArray = {
 	index: number;
 	status: string;
 	hover: boolean;
+	boatName?: string;
 }[];
 
 const Grid = () => {
 	const [tileArray, setTileArray] = useState<tileArray>([]);
 	const [gameStage, setGameStage] = useState("prep");
 	const [direction, setDirection] = useState<string>("x");
+	const [boat, setBoat] = useState<number>(0);
 
 	const GRID_SQUARES = 100;
 	const NUM_COORD = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
@@ -44,26 +46,29 @@ const Grid = () => {
 
 	// 	}
 	// }
+
 	const BOATSIZE = 4;
+
+	const boatArray = [
+		{ name: "Patrol Boat", size: 2 },
+		{ name: "Submarine", size: 3 },
+		{ name: "Destroyer", size: 3 },
+		{ name: "Battleship", size: 4 },
+		{ name: "Carrier", size: 5 },
+	];
 
 	const hoverHighlight = (boatIndex: number) => {
 		let updatedTileArray = [...tileArray];
-		if (
-			direction === "x" &&
-			((boatIndex + 10) % 10) + BOATSIZE <= 10 &&
-			tileArray[boatIndex + BOATSIZE].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
+		const boatSize = boatArray[boat].size;
+		let xBreak = checkPlacement("x", boatIndex, boatSize);
+		let yBreak = checkPlacement("y", boatIndex, boatSize);
+
+		if (direction === "x" && !xBreak) {
+			for (let i = 0; i < boatSize; i++) {
 				updatedTileArray[boatIndex + i].hover = true;
 			}
-		} else if (
-			direction === "y" &&
-			boatIndex + (BOATSIZE - 1) * 10 <= 99 &&
-			tileArray[boatIndex + (BOATSIZE - 1) * 10].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
+		} else if (direction === "y" && !yBreak) {
+			for (let i = 0; i < boatSize; i++) {
 				updatedTileArray[boatIndex + i * 10].hover = true;
 			}
 		}
@@ -73,22 +78,18 @@ const Grid = () => {
 
 	const highlightRemove = (boatIndex: number) => {
 		let updatedTileArray = [...tileArray];
-		if (
-			direction === "x" &&
-			((boatIndex + 10) % 10) + BOATSIZE <= 10 &&
-			tileArray[boatIndex + BOATSIZE].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
-				updatedTileArray[boatIndex + i].hover = false;
+		const boatSize = boatArray[boat].size;
+		let xBreak = checkPlacement("x", boatIndex, boatSize);
+		let yBreak = checkPlacement("y", boatIndex, boatSize);
+
+		if (direction === "x" && !xBreak) {
+			for (let i = 0; i < boatSize; i++) {
+				if (tileArray[boatIndex + i].hover === true) {
+					updatedTileArray[boatIndex + i].hover = false;
+				}
 			}
-		} else if (
-			direction === "y" &&
-			boatIndex + (BOATSIZE - 1) * 10 <= 99 &&
-			tileArray[boatIndex + (BOATSIZE - 1) * 10].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
+		} else if (direction === "y" && !yBreak) {
+			for (let i = 0; i < boatSize; i++) {
 				updatedTileArray[boatIndex + i * 10].hover = false;
 			}
 		}
@@ -98,30 +99,65 @@ const Grid = () => {
 
 	const placeShip = (boatIndex: number) => {
 		let updatedTileArray = [...tileArray];
-		if (
-			direction === "x" &&
-			((boatIndex + 10) % 10) + BOATSIZE <= 10 &&
-			tileArray[boatIndex + BOATSIZE].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
+		const boatSize = boatArray[boat].size;
+		const boatName = boatArray[boat].name;
+		let xBreak = checkPlacement("x", boatIndex, boatSize);
+		let yBreak = checkPlacement("y", boatIndex, boatSize);
+
+		if (direction === "x" && !xBreak) {
+			for (let i = 0; i < boatSize; i++) {
 				updatedTileArray[boatIndex + i].status = "boat";
+				updatedTileArray[boatIndex + i].boatName = boatName;
 				updatedTileArray[boatIndex + i].hover = false;
 			}
-		} else if (
-			direction === "y" &&
-			boatIndex + (BOATSIZE - 1) * 10 <= 99 &&
-			tileArray[boatIndex + (BOATSIZE - 1) * 10].status !== "boat" &&
-			tileArray[boatIndex].status !== "boat"
-		) {
-			for (let i = 0; i < BOATSIZE; i++) {
+
+			if (boat < boatArray.length - 1) {
+				let updatedIndex = boat + 1;
+				setBoat(updatedIndex);
+			}
+		} else if (direction === "y" && !yBreak) {
+			for (let i = 0; i < boatSize; i++) {
 				updatedTileArray[boatIndex + i * 10].status = "boat";
+				updatedTileArray[boatIndex + i * 10].boatName = boatName;
 				updatedTileArray[boatIndex + i * 10].hover = false;
+			}
+
+			if (boat < boatArray.length - 1) {
+				let updatedIndex = boat + 1;
+				setBoat(updatedIndex);
 			}
 		}
 
 		console.log(updatedTileArray);
 		setTileArray(updatedTileArray);
+	};
+
+	const checkPlacement = (
+		axis: string,
+		boatIndex: number,
+		boatSize: number
+	) => {
+		if (axis === "x") {
+			for (let i = 0; i < boatSize; i++) {
+				if (
+					((boatIndex + 10) % 10) + boatSize > 10 ||
+					tileArray[boatIndex + i].status === "boat"
+				) {
+					return true;
+				}
+			}
+		}
+
+		if (axis === "y") {
+			for (let i = 0; i < boatSize; i++) {
+				if (
+					boatIndex + (boatSize - 1) * 10 > 99 ||
+					tileArray[boatIndex + i * 10].status === "boat"
+				) {
+					return true;
+				}
+			}
+		}
 	};
 
 	return (
